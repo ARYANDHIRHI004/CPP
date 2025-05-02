ed645658382b27efdad07e3064a0ba1fc56af196
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-02 22:52:11 +0530

    Variable declearation and initialization

---
+++ second/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int score;
+    score = 100;
+
+    int balance = 5000;
+    cout << "Your bank balance is: " << balance << endl;
+    
+    const int uid = 45856;
+    cout << "Your ID is: " << uid << endl;
+    
+    return 0;
+}
