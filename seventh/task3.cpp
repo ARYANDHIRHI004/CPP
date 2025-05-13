6801008c8f64bd81f772e064a529ed91c34b5e95
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-05-13 09:54:52 +0530

    call by value and call by referance

---
+++ seventh/task3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void pourChai(int &cups){
+    cups = cups + 5;
+    cout << "Poured cups: " << cups << endl;
+}
+
+int main(){
+    
+    int chaiCupes = 2;
+    pourChai(chaiCupes);
+    cout << chaiCupes;
+
+    return 0;
+}
